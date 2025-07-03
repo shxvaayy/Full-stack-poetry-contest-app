96ba98523a60b34e82c0c5214b1d6d37eca6ae4a
Shivay Mehra <shivaym@Shivays-MacBook-Air-8.local>
2025-07-04 01:32:13 +0530

    Updated some code

--- client/src/pages/contact.tsx
+++ client/src/pages/contact.tsx
@@ -166,8 +166,7 @@ export default function ContactPage() {
                   <div>
                     <h3 className="font-semibold text-gray-900">Updates</h3>
                     <p className="text-sm text-gray-500">
-                      For information regarding result announcements, award ceremonies or theme updates,
-                      kindly check our news section under 'About Us'.
+                    For information regarding themes and challenges, award ceremonies or upcoming contests, kindly check our news section under ‘About Us’.
                     </p>
                   </div>
                 </div>
