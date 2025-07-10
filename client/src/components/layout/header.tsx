ab351ef4e132eb9c4edc251bf43dbc66ef5d0961
Shivay Mehra <shivaym@Shivays-MacBook-Air-8.local>
2025-07-10 15:26:24 +0530

    Start implementing edit features

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -148,7 +148,7 @@ export default function Header() {
 
           {/* Desktop Navigation - Perfectly Centered */}
           <nav className="hidden lg:flex absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <div className="flex items-center space-x-8">
+            <div className="flex items-center space-x-4">
               {navigation.map((item) => (
                 <Link
                   key={item.name}
